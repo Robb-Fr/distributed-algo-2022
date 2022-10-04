579d12d7c128cf84e61229d0d69c7fd664ec9b18
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-10-04 18:39:12 +0200

    Refactors for sending ACKs correctly

--- template_java/src/main/java/cs451/PerfectLink.java
+++ template_java/src/main/java/cs451/PerfectLink.java
@@ -32,28 +32,31 @@ public class PerfectLink {
     public void sendPerfect(Message message, Host dest) throws UnknownHostException, IOException, InterruptedException {
         Message m = null;
         do {
-            send(message, dest);
+            sendMessage(message, dest);
             Thread.sleep(2);
         } while ((m = receiveMessage()) == null || !m.isAckForMsg(message.getId()));
     }
 
-    public void receiveAndDeliver() throws InterruptedException, UnknownHostException, IOException {
+    public void receiveAndDeliver() {
         Message m = receiveMessage();
         if (m != null && !(delivered.contains(m)) && !m.isAck()) {
             parent.deliver(m);
             delivered.add(m);
-            send(new Message(m.getId(), thisHost, PayloadType.ACK), m.getSender());
         }
     }
 
-    private void send(Message message, Host dest) throws UnknownHostException, IOException {
+    private void sendMessage(Message message, Host dest) throws UnknownHostException, IOException {
         byte[] msgBytes = message.serialize();
+        if (msgBytes.length > Constants.MAX_DATAGRAM_LENGTH) {
+            throw new IndexOutOfBoundsException("Sent packet exceeds maximum accepted packet size");
+        }
         InetSocketAddress socketDest = new InetSocketAddress(dest.getInetAddress(), dest.getPort());
         DatagramPacket packet = new DatagramPacket(msgBytes, msgBytes.length, socketDest);
         socket.send(packet);
     }
 
     private Message receiveMessage() {
+        // we should only have sent packets not exceeding this size
         byte[] buf = new byte[Constants.MAX_DATAGRAM_LENGTH];
         DatagramPacket packet = new DatagramPacket(buf, Constants.MAX_DATAGRAM_LENGTH);
         try {
@@ -65,12 +68,20 @@ public class PerfectLink {
         }
         Message m = null;
         try {
-            m = Message.deserialize(buf);
+            m = Message.deserialize(packet.getData());
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
             System.err.println("Unable to deserialize the received message");
             return null;
         }
+        if (m != null) {
+            try {
+                sendMessage(new Message(m.getId(), thisHost, PayloadType.ACK), m.getSender());
+            } catch (IOException e) {
+                // failed send ACK, not a problem, we will retry when it is resent
+            }
+        }
+
         return m;
     }
 
