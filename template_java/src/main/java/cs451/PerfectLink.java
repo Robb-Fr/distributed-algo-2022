d5bae2f73a5bd1880ddab14d5d1525968649656e
Robb-Fr <git@robb-i.net>
2022-10-11 10:48:16 +0200

    Cleaner IO and moniplex link management

--- template_java/src/main/java/cs451/PerfectLink.java
+++ template_java/src/main/java/cs451/PerfectLink.java
@@ -4,29 +4,61 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import cs451.Message.PayloadType;
 
 import java.net.DatagramSocket;
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
 
-public class PerfectLink {
+public class PerfectLink implements Closeable {
     private final DatagramSocket socket;
     private final Host thisHost;
+    private final Map<Integer, Host> hosts;
     private final Deliverable parent;
-    private final HashSet<Message> delivered;
+    private final Set<Message> delivered;
 
-    public PerfectLink(Deliverable parent, Host thisHost) throws UnknownHostException, SocketException {
+    /**
+     * Constructor for a perfect link belonging to a receiver
+     * 
+     * @param parent
+     * @throws UnknownHostException
+     * @throws SocketException
+     */
+    public PerfectLink(Host thisHost, Deliverable parent) throws UnknownHostException, SocketException {
         if (parent == null || thisHost == null) {
-            throw new IllegalArgumentException("You cannot create a message with null fields");
+            throw new IllegalArgumentException("A receiver cannot have null parent or a null self host");
         }
-        InetAddress thisHostIp = InetAddress.getByName(thisHost.getIp());
         this.thisHost = thisHost;
+        InetAddress thisHostIp = InetAddress.getByName(thisHost.getIp());
         this.socket = new DatagramSocket(thisHost.getPort(), thisHostIp);
+        this.hosts = null;
         this.parent = parent;
         this.delivered = new HashSet<>();
+    }
+
+    /**
+     * Constructor for a perfect link belonging to a sender
+     * 
+     * @param thisHost
+     * @param hosts
+     * @throws SocketException
+     * @throws UnknownHostException
+     */
+    public PerfectLink(Host thisHost, Map<Integer, Host> hosts) throws SocketException, UnknownHostException {
+        if (thisHost == null || hosts == null) {
+            throw new IllegalArgumentException("A sender cannot have null self host or hosts map");
+        }
+        this.thisHost = thisHost;
+        InetAddress thisHostIp = InetAddress.getByName(thisHost.getIp());
+        this.socket = new DatagramSocket(thisHost.getPort(), thisHostIp);
+        this.hosts = hosts;
+        this.parent = null;
+        this.delivered = null;
     }
 
     /**
@@ -36,11 +68,12 @@ public class PerfectLink {
      * 
      * @param message : the message to be sent
      * @param dest    : the host that should receive the message
-     * @throws UnknownHostException
-     * @throws IOException
      * @throws InterruptedException
      */
-    public void sendPerfect(Message message, Host dest) throws UnknownHostException, IOException, InterruptedException {
+    public void sendPerfect(Message message, Host dest) throws InterruptedException {
+        if (thisHost == null || hosts == null) {
+            System.err.println("Cannot send through this perfect link as it belongs to a receiver");
+        }
         Message m = null;
         do {
             sendMessage(message, dest);
@@ -53,6 +86,9 @@ public class PerfectLink {
      * to the parent with the Validity, No duplication and No creation properties.
      */
     public void receiveAndDeliver() {
+        if (parent == null || delivered == null) {
+            System.err.println("Cannot deliver for this perfect link as it belongs to sender");
+        }
         Message m = receiveMessage();
         if (m != null && !(delivered.contains(m)) && !m.isAck()) {
             parent.deliver(m);
@@ -65,17 +101,23 @@ public class PerfectLink {
      * 
      * @param message
      * @param dest
-     * @throws UnknownHostException
-     * @throws IOException
      */
-    private void sendMessage(Message message, Host dest) throws UnknownHostException, IOException {
+    private void sendMessage(Message message, Host dest) {
         byte[] msgBytes = message.serialize();
+        if (msgBytes == null) {
+            return;
+        }
         if (msgBytes.length > Constants.MAX_DATAGRAM_LENGTH) {
             throw new IndexOutOfBoundsException("Sent packet exceeds maximum accepted packet size");
         }
         InetSocketAddress socketDest = new InetSocketAddress(dest.getInetAddress(), dest.getPort());
         DatagramPacket packet = new DatagramPacket(msgBytes, msgBytes.length, socketDest);
-        socket.send(packet);
+        try {
+            socket.send(packet);
+        } catch (IOException e) {
+            System.err.println("Error while sending the message");
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -87,31 +129,27 @@ public class PerfectLink {
     private Message receiveMessage() {
         // we should only have sent packets not exceeding this size
         byte[] buf = new byte[Constants.MAX_DATAGRAM_LENGTH];
-        DatagramPacket packet = new DatagramPacket(buf, Constants.MAX_DATAGRAM_LENGTH);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
         try {
             socket.receive(packet);
         } catch (IOException e) {
             // packet not delivered
+            System.err.println("Error while receiving the packet");
             e.printStackTrace();
-            return null;
         }
-        Message m = null;
-        try {
-            m = Message.deserialize(packet.getData());
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-            System.err.println("Unable to deserialize the received message");
-            return null;
-        }
+        Message m = Message.deserialize(packet.getData());
         if (m != null) {
-            try {
-                sendMessage(new Message(m.getId(), thisHost, PayloadType.ACK), m.getSender());
-            } catch (IOException e) {
-                // failed send ACK, not a problem, we will retry when it is resent
-            }
+            sendMessage(new Message(m.getId(), thisHost.getId(), PayloadType.ACK), hosts.get(m.getSenderId()));
+        }
+        return m;
+    }
+
+    @Override
+    public void close() {
+        if (socket != null) {
+            socket.close();
         }
 
-        return m;
     }
 
 }
