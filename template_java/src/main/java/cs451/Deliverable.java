58170a2433f14a2fadbafe23a250425ec39fd74c
Robb-Fr <git@robb-i.net>
2022-10-11 10:45:57 +0200

    Docs

--- template_java/src/main/java/cs451/Deliverable.java
+++ template_java/src/main/java/cs451/Deliverable.java
@@ -1,5 +1,10 @@
 package cs451;
 
 public interface Deliverable {
+    /**
+     * Delivers a message to a class
+     * 
+     * @param m : the message to be delivered
+     */
     public void deliver(Message m);
 }
