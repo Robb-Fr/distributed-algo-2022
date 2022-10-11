a2e0772ccdc5f16ea2ff63b5cc835e9c277c50e8
Robb-Fr <git@robb-i.net>
2022-10-11 10:45:51 +0200

    augments config parser for PerfectLink

--- template_java/src/main/java/cs451/ConfigParser.java
+++ template_java/src/main/java/cs451/ConfigParser.java
@@ -1,6 +1,9 @@
 package cs451;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ConfigParser {
 
@@ -14,6 +17,51 @@ public class ConfigParser {
 
     public String getPath() {
         return path;
+    }
+
+    public PerfectLinkConfig getPerfectLinkConfig() {
+        try {
+            BufferedReader myReader = new BufferedReader(new FileReader(path));
+            String data = myReader.readLine();
+            myReader.close();
+            if (data != null) {
+                String[] parameters = data.split(" ");
+                int nbMessages = Integer.parseInt(parameters[0]);
+                int receiverId = Integer.parseInt(parameters[1]);
+                return new PerfectLinkConfig(nbMessages, receiverId);
+            }
+        } catch (NumberFormatException e) {
+            System.err.println("Error occurred parse the parameters of the config");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("An error occurred while reading the config file");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public class PerfectLinkConfig {
+        private final int nbMessages;
+        private final int receiverId;
+
+        public PerfectLinkConfig(int nbMessages, int receiverId) {
+            this.nbMessages = nbMessages;
+            this.receiverId = receiverId;
+        }
+
+        public int getNbMessages() {
+            return nbMessages;
+        }
+
+        public int getReceiverId() {
+            return receiverId;
+        }
+
+        @Override
+        public String toString() {
+            return "PerfectLinkConfig [nbMessages=" + nbMessages + ", receiverId=" + receiverId + "]";
+        }
+
     }
 
 }
