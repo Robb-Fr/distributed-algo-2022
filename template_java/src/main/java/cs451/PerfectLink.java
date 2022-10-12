b1b710bb148261bdd6a55fabf6a35cc88f598850
Robb-Fr <git@robb-i.net>
2022-10-12 22:19:21 +0200

    Builds perfect link for handling sender and receiver

--- template_java/src/main/java/cs451/PerfectLink.java
+++ template_java/src/main/java/cs451/PerfectLink.java
@@ -2,10 +2,12 @@ package cs451;
 
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 
 import cs451.Message.PayloadType;
 
@@ -16,7 +18,7 @@ import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
 
 public class PerfectLink implements Closeable {
-    private final DatagramSocket socket;
+    private final AtomicReference<DatagramSocket> socket;
     private final Host thisHost;
     private final Map<Integer, Host> hosts;
     private final Deliverable parent;
@@ -25,18 +27,20 @@ public class PerfectLink implements Closeable {
     /**
      * Constructor for a perfect link belonging to a receiver
      * 
+     * @param thisHost
+     * @param hostsMap
      * @param parent
-     * @throws UnknownHostException
-     * @throws SocketException
+     * @param socket
      */
-    public PerfectLink(Host thisHost, Deliverable parent) throws UnknownHostException, SocketException {
-        if (parent == null || thisHost == null) {
-            throw new IllegalArgumentException("A receiver cannot have null parent or a null self host");
+    public PerfectLink(Host thisHost, Map<Integer, Host> hostsMap, Deliverable parent,
+            AtomicReference<DatagramSocket> socket) {
+        if (parent == null || hostsMap == null || thisHost == null || socket == null) {
+            throw new IllegalArgumentException(
+                    "A receiver cannot have null parent or a null self host or a null socket reference or null hosts");
         }
         this.thisHost = thisHost;
-        InetAddress thisHostIp = InetAddress.getByName(thisHost.getIp());
-        this.socket = new DatagramSocket(thisHost.getPort(), thisHostIp);
-        this.hosts = null;
+        this.hosts = hostsMap;
+        this.socket = socket;
         this.parent = parent;
         this.delivered = new HashSet<>();
     }
@@ -55,7 +59,8 @@ public class PerfectLink implements Closeable {
         }
         this.thisHost = thisHost;
         InetAddress thisHostIp = InetAddress.getByName(thisHost.getIp());
-        this.socket = new DatagramSocket(thisHost.getPort(), thisHostIp);
+        this.socket = new AtomicReference<DatagramSocket>(new DatagramSocket(thisHost.getPort(), thisHostIp));
+        this.socket.get().setSoTimeout(Constants.SOCKET_TIMEOUT);
         this.hosts = hosts;
         this.parent = null;
         this.delivered = null;
@@ -77,8 +82,10 @@ public class PerfectLink implements Closeable {
         Message m = null;
         do {
             sendMessage(message, dest);
-            Thread.sleep(2);
+            Thread.sleep(Constants.SLEEP_BEFORE_RESEND);
+            System.out.println("Message sent");
         } while ((m = receiveMessage()) == null || !m.isAckForMsg(message.getId()));
+        System.out.println("Perfectly sent");
     }
 
     /**
@@ -90,10 +97,26 @@ public class PerfectLink implements Closeable {
             System.err.println("Cannot deliver for this perfect link as it belongs to sender");
         }
         Message m = receiveMessage();
+        if (m != null) {
+            System.out.println("Received message : " + m);
+        }
         if (m != null && !(delivered.contains(m)) && !m.isAck()) {
             parent.deliver(m);
             delivered.add(m);
+            System.out.println("Delivered message : " + m);
         }
+    }
+
+    @Override
+    public void close() {
+        if (socket != null) {
+            socket.get().close();
+        }
+
+    }
+
+    public AtomicReference<DatagramSocket> getSocket() {
+        return socket;
     }
 
     /**
@@ -113,7 +136,7 @@ public class PerfectLink implements Closeable {
         InetSocketAddress socketDest = new InetSocketAddress(dest.getInetAddress(), dest.getPort());
         DatagramPacket packet = new DatagramPacket(msgBytes, msgBytes.length, socketDest);
         try {
-            socket.send(packet);
+            socket.get().send(packet);
         } catch (IOException e) {
             System.err.println("Error while sending the message");
             e.printStackTrace();
@@ -131,25 +154,20 @@ public class PerfectLink implements Closeable {
         byte[] buf = new byte[Constants.MAX_DATAGRAM_LENGTH];
         DatagramPacket packet = new DatagramPacket(buf, buf.length);
         try {
-            socket.receive(packet);
+            socket.get().receive(packet);
         } catch (IOException e) {
-            // packet not delivered
-            System.err.println("Error while receiving the packet");
-            e.printStackTrace();
+            // packet not delivered, report only the non Timeout error
+            if (!(e instanceof SocketTimeoutException)) {
+                System.err.println("Error while receiving the packet");
+                e.printStackTrace();
+            }
         }
         Message m = Message.deserialize(packet.getData());
-        if (m != null) {
+        if (m != null && !m.isAck()) {
+            System.out.println("Sending ACK");
             sendMessage(new Message(m.getId(), thisHost.getId(), PayloadType.ACK), hosts.get(m.getSenderId()));
         }
         return m;
-    }
-
-    @Override
-    public void close() {
-        if (socket != null) {
-            socket.close();
-        }
-
     }
 
 }
