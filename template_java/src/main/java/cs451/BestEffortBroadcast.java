45d54ac254a0cbdb5c1571dfabb510bb347d3fa6
Robb-Fr <git@robb-i.net>
2022-11-01 18:46:14 +0100

    documents and deal with self delivering

--- template_java/src/main/java/cs451/BestEffortBroadcast.java
+++ template_java/src/main/java/cs451/BestEffortBroadcast.java
@@ -6,40 +6,74 @@ import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class BestEffortBroadcast implements Deliverable {
+public class BestEffortBroadcast implements Deliverable, SocketGiver {
+    private final int myId;
     private final PerfectLink link;
     private final Map<Integer, Host> hostsMap;
     private final Deliverable parent;
 
-    public BestEffortBroadcast(byte myId, Map<Integer, Host> hostsMap, Deliverable parent,
+    /**
+     * Constructor for a Beb to be given to a receiver
+     * 
+     * @param myId
+     * @param hostsMap
+     * @param parent
+     * @param socket
+     */
+    public BestEffortBroadcast(int myId, Map<Integer, Host> hostsMap, Deliverable parent,
             AtomicReference<DatagramSocket> socket) {
         if (parent == null || hostsMap == null || socket == null) {
             throw new IllegalArgumentException(
                     "A receiver cannot have null parent or a null self host or a null socket reference or null hosts");
         }
+        this.myId = myId;
         this.link = new PerfectLink(myId, hostsMap, this, socket);
         this.hostsMap = hostsMap;
         this.parent = parent;
     }
 
-    public BestEffortBroadcast(byte myId, Map<Integer, Host> hostsMap) throws SocketException, UnknownHostException {
+    /**
+     * Constructor for a Beb to be given to a sender
+     * 
+     * @param myId
+     * @param hostsMap
+     * @throws SocketException
+     * @throws UnknownHostException
+     */
+    public BestEffortBroadcast(int myId, Map<Integer, Host> hostsMap) throws SocketException, UnknownHostException {
         if (hostsMap == null) {
             throw new IllegalArgumentException("A sender cannot have null self host or hosts map");
         }
+        this.myId = myId;
         this.link = new PerfectLink(myId, hostsMap);
         this.hostsMap = hostsMap;
         this.parent = null;
     }
 
     public void broadcast(Message m) throws InterruptedException {
+        if (m == null) {
+            throw new IllegalArgumentException("Cannot broadcast a null message");
+        }
         for (Host host : hostsMap.values()) {
-            link.sendPerfect(m, host);
+            if (host.getId() == myId) {
+                deliver(m);
+            } else {
+                link.sendPerfect(m, host);
+            }
         }
     }
 
     @Override
     public void deliver(Message m) {
+        if (parent == null) {
+            throw new IllegalAccessError("Cannot access the parent of a sender");
+        }
         parent.deliver(m);
+    }
+
+    @Override
+    public AtomicReference<DatagramSocket> getSocket() {
+        return link.getSocket();
     }
 
 }
