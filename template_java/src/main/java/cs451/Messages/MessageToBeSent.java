913fc4c0b267739863a6f9b69b53904e73314704
Robb-Fr <git@robb-i.net>
2022-12-06 14:52:30 +0100

    Adapts for part 3

--- template_java/src/main/java/cs451/Messages/MessageToBeSent.java
+++ template_java/src/main/java/cs451/Messages/MessageToBeSent.java
@@ -4,7 +4,6 @@ public class MessageToBeSent {
     private final Message message;
     private final byte[] serializedMsg;
     private final short dest;
-    private final MessageTupleWithSender ackForThisMessage;
 
     public MessageToBeSent(Message m, short dest) {
         if (m == null) {
@@ -12,6 +11,7 @@ public class MessageToBeSent {
         }
 
         byte[] msgBytes = m.serialize();
+
         if (msgBytes == null) {
             throw new IllegalArgumentException("Could not serialize message m");
         }
@@ -19,15 +19,10 @@ public class MessageToBeSent {
         this.serializedMsg = msgBytes;
         this.dest = dest;
         this.message = m;
-        this.ackForThisMessage = m.ackForThisMsgTupleWithSender(dest);
     }
 
     public Message getMessage() {
         return message;
-    }
-
-    public MessageTupleWithSender getAckForThisMessage() {
-        return ackForThisMessage;
     }
 
     public byte[] getSerializedMsg() {
