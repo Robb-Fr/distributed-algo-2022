ccb2472c8ca266c9b2a52bf8fe384f0e3a30268d
Robb-Fr <git@robb-i.net>
2022-12-13 19:09:27 +0100

    adds sender code

--- template_java/src/main/java/cs451/Sender.java
+++ template_java/src/main/java/cs451/Sender.java
@@ -1,31 +1,55 @@
 package cs451;
 
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.List;
 import java.util.Map;
-import cs451.Messages.LogsBuilder;
+import java.util.Set;
+
+import cs451.Broadcasts.LatticeAgreement;
 import cs451.Parsers.ConfigParser;
 import cs451.Parsers.ConfigParser.LatticeConfig;
+import cs451.States.LatticeState;
+import cs451.States.LatticeStateGiver;
 import cs451.States.PlState;
 import cs451.States.PlStateGiver;
 
-public class Sender implements Runnable, PlStateGiver {
-    private final LogsBuilder logsBuilder;
-    private final short myId;
+public class Sender implements Runnable, PlStateGiver, LatticeStateGiver {
     private final LatticeConfig latticeConfig;
+    private final LatticeAgreement latticeAgreement;
 
-    public Sender(LogsBuilder logsBuilder, short myId, Map<Short, Host> hostsMap,
-            ConfigParser config) {
-        this.myId = myId;
-        this.logsBuilder = logsBuilder;
+    public Sender(short myId, Map<Short, Host> hostsMap,
+            ConfigParser config) throws SocketException, UnknownHostException {
         this.latticeConfig = config.getLatticeConfig();
+        this.latticeAgreement = new LatticeAgreement(myId, hostsMap, latticeConfig);
     }
 
     @Override
     public void run() {
-        System.err.println("Not implemented !");
+        Thread latticeSender = new Thread(latticeAgreement, "Lattice Receiver");
+        latticeSender.start();
+        try {
+            List<Set<Integer>> proposals = latticeConfig.getProposals();
+            for (int i = 0; i < proposals.size(); ++i) {
+                Set<Integer> proposal = proposals.get(i);
+                while (!latticeAgreement.propose(i, proposal)) {
+                    Thread.sleep(Constants.SLEEP_BEFORE_NEXT_POLL);
+                }
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Interrupted sender while sleeping");
+            latticeSender.interrupt();
+            e.printStackTrace();
+        }
     }
 
     @Override
     public PlState getPlState() {
-        return null;
+        return latticeAgreement.getPlState();
+    }
+
+    @Override
+    public LatticeState getLatticeState() {
+        return latticeAgreement.getLatticeState();
     }
 }
