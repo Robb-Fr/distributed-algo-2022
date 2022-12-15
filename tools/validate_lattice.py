81d624c680608eaf0650c4e561103bbe5395ecd4
Robb-Fr <git@robb-i.net>
2022-12-15 16:57:45 +0100

    creates validation script

--- tools/validate_lattice.py
+++ tools/validate_lattice.py
@@ -1,18 +1,89 @@
 from pathlib import Path
-from sys import argv
+from sys import argv, stderr
+from os.path import join
+from itertools import combinations
 
-directory = "./logs"
-nb_hosts = int(argv[1]) if len(argv) > 0 else 0
-files_to_get = [
-    "proc" + ("0" if i < 10 else "") + f"{i}.output" for i in range(1, nb_hosts + 1)
+
+def sets_in_file(filename: str) -> list[set[int]]:
+    with open(filename, "r") as file:
+        return [
+            {int(e) for e in line.strip().split(" ") if e != "" and line.strip() != ""}
+            for line in file.readlines()
+        ]
+
+
+def get_filename_for_proc(parent_dir: str, proc_nb: int, extension: str) -> str:
+    return join(
+        parent_dir, "proc" + ("0" if proc_nb < 10 else "") + f"{proc_nb}." + extension
+    )
+
+
+def was_stopped_early(decided_values: list[set[int]], expected_len: int) -> bool:
+    # we assume that if a process was stopped early, it does not have enough values in decided set
+    return len(decided_values) < expected_len
+
+
+logs_dir = "logs"
+if not Path(logs_dir).is_dir():
+    print(
+        "Cannot open the logs dir, you can modify it in the script's code (./logs/ directory expected)"
+    )
+    raise SystemExit(1)
+
+if len(argv) != 3 or int(argv[1]) < 1 or int(argv[2]) < 1:
+    print("Usage : python3 validate_lattice.py NB_HOSTS NB_AGREEMENTS")
+    raise SystemExit(1)
+
+nb_hosts = int(argv[1])
+nb_agreements = int(argv[2])
+output_files = [
+    get_filename_for_proc(logs_dir, i, "output") for i in range(1, nb_hosts + 1)
+]
+config_files = [
+    get_filename_for_proc(logs_dir, i, "config") for i in range(1, nb_hosts + 1)
 ]
 
+proposals = [sets_in_file(f)[1:] for f in config_files]  # all process' proposals
+decided = [sets_in_file(f) for f in output_files]  # all process'decided values
+failed = False
 
-def get_delivered_set(filename: str) -> list[set[int]]:
-    with open(filename) as file:
-        return [{int(e) for e in l.split(" ")} for l in file.readline()]
+for agreement in range(nb_agreements):
+    all_proposals = set()
+    for prop in proposals:
+        # makes union of all proposals over all hosts for this agreement
+        all_proposals = all_proposals.union(prop[agreement])
+    for host, (prop, dec) in enumerate(zip(proposals, decided), start=1):
+        if not (
+            was_stopped_early(dec, nb_agreements)
+            or prop[agreement].issubset(dec[agreement])
+        ):
+            print(
+                f"Failed to verify Validity 1 for agreement : {agreement} at host : {host}",
+                stderr,
+            )
+            failed = True
 
+        if not (
+            was_stopped_early(dec, nb_agreements)
+            or dec[agreement].issubset(all_proposals)
+        ):
+            print(
+                f"Failed to verify Validity 2 for agreement : {agreement} at host : {host}",
+                stderr,
+            )
+            failed = True
+    for dec1, dec2 in combinations(decided, 2):
+        if not (
+            was_stopped_early(dec1, nb_agreements)
+            or was_stopped_early(dec2, nb_agreements)
+            or dec1[agreement].issubset(dec2[agreement])
+            or dec2[agreement].issubset(dec1[agreement])
+        ):
+            print(
+                f"Failed to verify Consistency for agreement : {agreement} at host : {host}",
+                stderr,
+            )
+            failed = True
 
-for f in Path(directory).iterdir():
-    if f.name in files_to_get:
-        print(f.name)
+if not failed:
+    print("Successfully validated all tests")
